0950c71384918751b5959bb3b370e60b5ae641ad
TEO-SMC <tom.eschment@googlemail.com>
2012-07-20 16:58:19 +0300

    Update zoll_zentimeter_umrechner.py

--- zoll_zentimeter_umrechner.py
+++ zoll_zentimeter_umrechner.py
@@ -7,7 +7,7 @@ print("Zoll zu Zentimeter Umrechner (C) Tom Eschment")
 #Var. definition
 fehler = 1
 
-#While-Schleife 1
+#While-Schleife 
 while fehler == 1:
 
 #Vorauswahl
@@ -27,24 +27,20 @@ while fehler == 1:
     if vorauswahlz == 1:
         print("Bitte geben Sie den Zentimeter-Wert an, welcher in Zoll umgewandelt werden soll, um das Programm zu beenden geben Sie bitte den Wert 0 an.")
         z = input()
-        fehler = 2
     elif vorauswahlz == 2:
         print("Bitte geben Sie den Zoll-Wert an, welcher in Zentimeter umgewandelt werden soll, um das Programm zu beenden geben Sie bitte den Wert 0 an.")
         z = input()
-        fehler = 2
 
 #Ende 1
     elif vorauswahlz == 0:
         print("Sie haben das Programm beendet")
         fehler = 0
+        break
 
 #Fehlermeldung 1
     else:
         print("Sie haben keinen passenden Wert angegeben bitte versuchen Sie es erneut")
         fehler = 1
-
-#Whiler-Schleife 2
-while fehler == 2:
 
 #Versuch Eingabe in Zahl umzuwandeln 2
     try:
@@ -54,6 +50,7 @@ while fehler == 2:
         if zahl == 0:
             print("Sie haben das Programm beendet")
             fehler = 0
+            break
 
 #Verzweigung 2
         elif vorauswahlz == 1:
@@ -61,14 +58,12 @@ while fehler == 2:
 #Ausgabe des Umgewandelten Wertes 1
             print(zahl, "cm =", zahl/2.54, "Zoll")
             fehler = 1
-            break
         
 #Ausgabe des Umgewandeltes Wertes 2
         else:
             print(zahl, "Zoll =", zahl*2.54, "cm")
             fehler = 1
-            break
-            
+        
 #Fehlermeldung 2
     except:
         print("Sie haben keine Zahl eingegeben, bitte versuchen sie es erneut.")
